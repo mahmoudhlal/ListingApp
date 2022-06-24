110142c7573e7aaa75aef86f68fbbcb0d2994769
ma7moud <mahmoudhlal741996@gmail.com>
2022-06-24 10:36:42 +0200

    create ListingApiResponse.kt work as response data model

---
+++ data/src/main/java/com/example/data/datamodel/ListingApiResponse.kt
@@ -0,0 +1,20 @@
+package com.example.data.datamodel
+
+data class ListingApiResponse(
+    val pagination: Pagination,
+    val results: List<Listing>
+)
+
+data class Listing(
+    val created_at: String,
+    val image_ids: List<String>,
+    val image_urls: List<String>,
+    val image_urls_thumbnails: List<String>,
+    val name: String,
+    val price: String,
+    val uid: String
+)
+
+data class Pagination(
+    val key: Any
+)
