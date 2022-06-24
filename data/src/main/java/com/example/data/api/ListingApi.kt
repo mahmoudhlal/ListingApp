25c7dcfb433f557bf81a093a8490b0e00a87fa4f
ma7moud <mahmoudhlal741996@gmail.com>
2022-06-24 10:40:57 +0200

    create GET function api service called getListings to fetch data from backend

--- data/src/main/java/com/example/data/api/ListingApi.kt
+++ data/src/main/java/com/example/data/api/ListingApi.kt
@@ -1,4 +1,13 @@
 package com.example.data.api
 
+import com.example.data.datamodel.ListingApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ListingApi {
+
+    @GET("dynamodb-writer")
+    suspend fun getListings(): Response<ListingApiResponse>
+
 }
